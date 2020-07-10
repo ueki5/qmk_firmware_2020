de30583e66e9a73490e7042ef081d39fb8620fc9
ueki5 <ueki005@gmail.com>
2020-07-10 15:58:39 +0900

    update Right Fn layer

--- keyboards/hhkb/ansi/keymaps/ueki5/keymap.c
+++ keyboards/hhkb/ansi/keymaps/ueki5/keymap.c
@@ -20,21 +20,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    /* BASE Level: Default Layer
-     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
-     | Esc   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | -     | =   | \     | ` |
-     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
-     | Tab   | Q | W | E | R | T | Y | U | I | O | P | [     | ]   | Backs |
-     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------|
-     | Cont  | A | S | D | F | G | H | J | K | L | ; | '     | Ent |
-     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----|
-     | Shift | Z | X | C | V | B | N | M | , | . | / | Shift | RGUI|
-     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----|
-
-            |------+------+-----------------------+------+------|
-            | LAlt | Fn0  | ******* Space ******* | Fn1  | RAlt |
-            |------+------+-----------------------+------+------|
-    */
+    /* BASE Level: Default Layer */
 
     [BASE] = LAYOUT( //  default layer
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,
@@ -43,22 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_RGUI,
         KC_LALT, LT(HHKB1, JP_MHEN), /*        */ KC_SPC, LT(HHKB2, JP_HENK), KC_RALT),
 
-    /* Layer HHKB1: Left Fn
-      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |      | F1  | F2  | F3  | F4 | F5 | F6 | F7 | F8  | F9  | F10 | F11 | F12   | Ins   |     |
-      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |      |     |     |     |    |    | Top| Hom| Up  | End | Btom|JPUNS|JPPIPE |       |
-      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------|
-      |      |Ctl-A|Ctl-S|LCtrl|LShf|    | BS | Lef| Dow | Rig | Del |JPBSL|       |
-      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------|
-      |      |Ctl-Z|Ctl-X|Ctl-C|CtlV|    |Esc |PgDn| PSCR| PgU |     |     |       |
-      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------|
-
-                 |------+------+----------------------+------+------+
-                 | **** | **** | ******* Enter ****** | **** | **** |
-                 |------+------+----------------------+------+------+
-
-     */
+    /* Layer HHKB1: Left Fn */
 
     [HHKB1] = LAYOUT(
         KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS, KC_TRNS,
@@ -67,27 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, LCTLZ  , LCTLX,   LCTLC,   LCTLV,   KC_TRNS, KC_ESC,  KC_PGDN, KC_PSCR, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_ENT , KC_TRNS, KC_TRNS),
 
-    /* Layer HHKB2: Right Fn
-      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |      | F1  | F2  | F3  | F4 | F5 | F6 | F7 | F8  | F9  | F10 | F11 | F12   |       |     |
-      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |      |WH_U |BTN1 |MS_U |BTN2| Esc|    |    |     |     |     |     |       |       |
-      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------|
-      |      |WH_L |MS_L |MS_D |MS_R|WH_R|BSLS|RShf|RCtrl|     |     |     |       |
-      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------|
-      |      | BSLS| EQL |MINS |PPLS|WH_D|PPLS|MINS|EQL  |     |     |     |       |
-      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------|
-
-                 |------+------+----------------------+------+------+
-                 | **** | **** | ******* Enter ****** | **** | **** |
-                 |------+------+----------------------+------+------+
-
-     */
+    /* Layer HHKB2: Right Fn */
 
     [HHKB2] = LAYOUT(
-        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_ESC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R, KC_BSLS, KC_RSFT, KC_RCTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_BSLS, KC_EQL,  KC_MINS, KC_PPLS, KC_WH_D, KC_PPLS, KC_MINS, KC_EQL,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_ENT , KC_TRNS, KC_TRNS)};
-
+        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_LCBR, KC_RCBR, KC_MINS, KC_UNDS, KC_PIPE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_LBRC, KC_RBRC, KC_EQL , KC_PLUS, KC_BSLS, KC_TRNS, KC_RSFT, KC_RCTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_QUOT, KC_DQUO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)};
