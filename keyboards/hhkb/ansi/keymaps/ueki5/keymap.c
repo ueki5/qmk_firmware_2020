d0ec8d2bc8b3a9f9f6eeff438434d08ac1c92fb4
ueki5 <ueki005@gmail.com>
2020-07-10 16:01:13 +0900

    update

--- keyboards/hhkb/ansi/keymaps/ueki5/keymap.c
+++ keyboards/hhkb/ansi/keymaps/ueki5/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_RGUI,
-        KC_LALT, LT(HHKB1, JP_MHEN), /*        */ KC_SPC, LT(HHKB2, JP_HENK), KC_RALT),
+        KC_LALT, LT(HHKB1, JP_MHEN), KC_SPC, LT(HHKB2, JP_HENK), KC_RALT),
 
     /* Layer HHKB1: Left Fn */
 
