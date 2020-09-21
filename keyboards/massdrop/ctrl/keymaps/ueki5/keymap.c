2e3a8f5dbe41719f6b556fc2f87900e25d7598be
ueki5 <ueki005@gmail.com>
2020-09-21 19:18:37 +0900

    make every modifier key Left side

--- keyboards/massdrop/ctrl/keymaps/ueki5/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/ueki5/keymap.c
@@ -42,7 +42,7 @@ enum combos {
 };
 
 // @   ^&*()_+|~
-const uint16_t PROGMEM combo_01[] = {KC_RSFT, KC_2   , COMBO_END};
+const uint16_t PROGMEM combo_01[] = {KC_LSFT, KC_2   , COMBO_END};
 const uint16_t PROGMEM combo_02[] = {KC_LSFT, KC_6   , COMBO_END};
 const uint16_t PROGMEM combo_03[] = {KC_LSFT, KC_7   , COMBO_END};
 const uint16_t PROGMEM combo_04[] = {KC_LSFT, KC_8   , COMBO_END};
@@ -111,8 +111,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         JP_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, JP_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_LBRC, JP_RBRC, KC_JYEN,   KC_DEL,  KC_END,  KC_PGDN, \
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_QUOT, KC_ENT, \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                              KC_UP, \
-        MO(3),   KC_LALT, LT(1, JP_MHEN),   KC_SPC,                    LT(2, JP_HENK),   KC_RALT, KC_RGUI, KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,                              KC_UP, \
+        MO(3),   KC_LALT, LT(1, JP_MHEN),   KC_SPC,                    LT(2, JP_HENK),   KC_LALT, KC_LGUI, KC_LCTL,            KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [1] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______, \
