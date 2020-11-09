a186deaaccf4212ff49ebdddc01013102f60b9dd
ueki5 <ueki005@gmail.com>
2020-11-09 23:31:30 +0900

    change keymap

--- keyboards/preonic/keymaps/ueki5/keymap.c
+++ keyboards/preonic/keymaps/ueki5/keymap.c
@@ -147,8 +147,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Raise */
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12, \
-  _______, JP_LBRC, JP_RBRC, JP_MINS, JP_UNDS, JP_GRV,  _______, _______, _______, _______, _______, _______,  \
-  _______, JP_LCBR, JP_RCBR, JP_EQL , JP_PLUS, JP_TILD, _______, KC_LSFT, KC_RCTL, _______, _______, _______, \
+  _______, JP_MINS,JP_UNDS,  JP_LCBR, JP_RCBR, JP_GRV,  _______, _______, _______, _______, _______, _______,  \
+  _______, JP_EQL, JP_PLUS,  JP_LBRC, JP_RBRC, JP_TILD, _______, KC_LSFT, KC_RCTL, _______, _______, _______, \
   _______, KC_TRNS, KC_TRNS, JP_QUOT, JP_DQUO, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, KC_ENT,  KC_ENT,  _______, _______, _______, _______, _______ \
 ),
