8a49c558ec0fea06cb19e3b7b505d981241ee938
ueki5 <ueki005@gmail.com>
2021-09-27 21:16:57 +0900

    us-jpを切り出し

---
+++ keyboards/preonic/keymaps/us-jp/config.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+    #define STARTUP_SONG SONG(PREONIC_SOUND)
+    // #define STARTUP_SONG SONG(NO_SOUND)
+
+    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
+                                  SONG(COLEMAK_SOUND), \
+                                  SONG(DVORAK_SOUND) \
+                                }
+#endif
+
+#define MUSIC_MASK (keycode != KC_NO)
+
+/*
+ * MIDI options
+ */
+
+/* Prevent use of disabled MIDI features in the keymap */
+//#define MIDI_ENABLE_STRICT 1
+
+/* enable basic MIDI features:
+   - MIDI notes can be sent when in Music mode is on
+*/
+
+#define MIDI_BASIC
+
+/* enable advanced MIDI features:
+   - MIDI notes can be added to the keymap
+   - Octave shift and transpose
+   - Virtual sustain, portamento, and modulation wheel
+   - etc.
+*/
+//#define MIDI_ADVANCED
+
+/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
+//#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+// tap and hold
+#define TAPPING_TERM    200
+#define PERMISSIVE_HOLD
+#define RETRO_TAPPING
+
+// key combo
+#define COMBO_COUNT 14
+#define COMBO_TERM  1000
+#define EXTRA_EXTRA_LONG_COMBOS
---
+++ keyboards/preonic/keymaps/us-jp/keymap.c
@@ -0,0 +1,296 @@
+/* Copyright 2015-2017 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "muse.h"
+#include "keymap_jp.h"
+
+enum preonic_layers {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _ADJUST
+};
+
+enum preonic_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  BACKLIT
+};
+
+// Original Key define
+#define LCTLA LCTL(KC_A)
+#define LCTLC LCTL(KC_C)
+#define LCTLS LCTL(KC_S)
+#define LCTLV LCTL(KC_V)
+#define LCTLX LCTL(KC_X)
+#define LCTLZ LCTL(KC_Z)
+#define U_TOP LCTL(KC_HOME)
+#define U_BTM LCTL(KC_END)
+
+// key Combo definition
+enum combos {
+  COMBO_01,
+  COMBO_02,
+  COMBO_03,
+  COMBO_04,
+  COMBO_05,
+  COMBO_06,
+  COMBO_07,
+  COMBO_08,
+  COMBO_09,
+  COMBO_10,
+  COMBO_11,
+  COMBO_12,
+  COMBO_13,
+  COMBO_14
+};
+
+// @   ^&*()_+|~
+const uint16_t PROGMEM combo_01[] = {KC_LSFT, KC_2   , COMBO_END};
+const uint16_t PROGMEM combo_02[] = {KC_LSFT, KC_6   , COMBO_END};
+const uint16_t PROGMEM combo_03[] = {KC_LSFT, KC_7   , COMBO_END};
+const uint16_t PROGMEM combo_04[] = {KC_LSFT, KC_8   , COMBO_END};
+const uint16_t PROGMEM combo_05[] = {KC_LSFT, KC_9   , COMBO_END};
+const uint16_t PROGMEM combo_06[] = {KC_LSFT, KC_0   , COMBO_END};
+const uint16_t PROGMEM combo_07[] = {KC_LSFT, KC_MINS, COMBO_END};
+const uint16_t PROGMEM combo_08[] = {KC_LSFT, JP_EQL , COMBO_END};
+const uint16_t PROGMEM combo_09[] = {KC_LSFT, KC_JYEN, COMBO_END};
+const uint16_t PROGMEM combo_10[] = {KC_LSFT, JP_GRV , COMBO_END};
+// {}
+const uint16_t PROGMEM combo_11[] = {KC_LSFT, JP_LBRC, COMBO_END};
+const uint16_t PROGMEM combo_12[] = {KC_LSFT, JP_RBRC, COMBO_END};
+// :"
+const uint16_t PROGMEM combo_13[] = {KC_LSFT, KC_SCLN, COMBO_END};
+const uint16_t PROGMEM combo_14[] = {KC_LSFT, JP_QUOT, COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+  // @   ^&*()_+|~
+  [COMBO_01] = COMBO(combo_01, JP_AT),
+  [COMBO_02] = COMBO(combo_02, JP_CIRC),
+  [COMBO_03] = COMBO(combo_03, JP_AMPR),
+  [COMBO_04] = COMBO(combo_04, JP_ASTR),
+  [COMBO_05] = COMBO(combo_05, JP_LPRN),
+  [COMBO_06] = COMBO(combo_06, JP_RPRN),
+  [COMBO_07] = COMBO(combo_07, JP_UNDS),
+  [COMBO_08] = COMBO(combo_08, JP_PLUS),
+  [COMBO_09] = COMBO(combo_09, JP_PIPE),
+  [COMBO_10] = COMBO(combo_10, JP_TILD),
+  // {}
+  [COMBO_11] = COMBO(combo_11, JP_LCBR),
+  [COMBO_12] = COMBO(combo_12, JP_RCBR),
+  // :"
+  [COMBO_13] = COMBO(combo_13, JP_COLN),
+  [COMBO_14] = COMBO(combo_14, JP_DQUO)
+};
+void process_combo_event(uint8_t combo_index, bool pressed) {
+// dprintf("process_combo_event combo_index=%u,pressed=%u\n", combo_index, pressed);
+  switch(combo_index) {
+    // case COMBO_01:
+    //   if (pressed) {
+    //     tap_code16(LSFT(KC_C));
+    //   }
+    //   break;
+    // case COMBO_02:
+    //   if (pressed) {
+    //     tap_code16(LSFT(KC_D));
+    //   }
+    //   break;
+    // case COMBO_03:
+    //   if (pressed) {
+    //     tap_code16(LSFT(KC_E));
+    //   }
+    //   break;
+  }
+}
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  //debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
+}
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty */
+[_QWERTY] = LAYOUT_preonic_grid( \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_JYEN, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
+  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,  \
+  MO(_ADJUST),  KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, JP_MHEN),   KC_SPC,  KC_SPC,  LT(_RAISE, JP_HENK),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+),
+
+/* Lower */
+[_LOWER] = LAYOUT_preonic_grid( \
+  KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12, \
+  _______, _______, _______, _______, _______, _______, U_TOP,   KC_HOME, KC_UP,   KC_END,  U_BTM,   _______,  \
+  _______, LCTLA,   LCTLS,   KC_LCTL, KC_LSFT, _______, KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  _______, \
+  _______, LCTLZ,   LCTLX,   LCTLC,   LCTLV,   _______, KC_ESC,  KC_PGDN, KC_PSCR, KC_PGUP, _______, _______, \
+  _______, _______, _______, _______, _______, KC_ENT,  KC_ENT,  _______, _______, _______, _______, _______ \
+),
+
+/* Raise */
+[_RAISE] = LAYOUT_preonic_grid( \
+  KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12, \
+  _______, JP_MINS,JP_UNDS,  JP_LCBR, JP_RCBR, JP_GRV,  _______, _______, _______, _______, _______, _______,  \
+  _______, JP_EQL, JP_PLUS,  JP_LBRC, JP_RBRC, JP_TILD, _______, KC_LSFT, KC_RCTL, _______, _______, _______, \
+  _______, KC_TRNS, KC_TRNS, JP_QUOT, JP_DQUO, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, KC_ENT,  KC_ENT,  _______, _______, _______, _______, _______ \
+),
+
+/* Adjust */
+[_ADJUST] = LAYOUT_preonic_grid( \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
+  _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,  \
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, _______,  _______, _______, \
+  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, JP_ZKHK, JP_ZKHK, _______, _______, _______, _______, _______  \
+)
+
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+        case QWERTY:
+          if (record->event.pressed) {
+            set_single_persistent_default_layer(_QWERTY);
+          }
+          return false;
+          break;
+        case LOWER:
+          if (record->event.pressed) {
+            layer_on(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          } else {
+            layer_off(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          }
+          return false;
+          break;
+        case RAISE:
+          if (record->event.pressed) {
+            layer_on(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          } else {
+            layer_off(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+          }
+          return false;
+          break;
+        case BACKLIT:
+          if (record->event.pressed) {
+            register_code(KC_LSFT);
+            #ifdef BACKLIGHT_ENABLE
+              backlight_step();
+            #endif
+            #ifdef __AVR__
+            writePinLow(E6);
+            #endif
+          } else {
+            unregister_code(KC_LSFT);
+            #ifdef __AVR__
+            writePinHigh(E6);
+            #endif
+          }
+          return false;
+          break;
+      }
+    return true;
+};
+
+bool muse_mode = false;
+uint8_t last_muse_note = 0;
+uint16_t muse_counter = 0;
+uint8_t muse_offset = 70;
+uint16_t muse_tempo = 50;
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+  if (muse_mode) {
+    if (IS_LAYER_ON(_RAISE)) {
+      if (clockwise) {
+        muse_offset++;
+      } else {
+        muse_offset--;
+      }
+    } else {
+      if (clockwise) {
+        muse_tempo+=1;
+      } else {
+        muse_tempo-=1;
+      }
+    }
+  } else {
+    if (clockwise) {
+      register_code(KC_PGDN);
+      unregister_code(KC_PGDN);
+    } else {
+      register_code(KC_PGUP);
+      unregister_code(KC_PGUP);
+    }
+  }
+}
+
+void dip_switch_update_user(uint8_t index, bool active) {
+    switch (index) {
+        case 0:
+            if (active) {
+                layer_on(_ADJUST);
+            } else {
+                layer_off(_ADJUST);
+            }
+            break;
+        case 1:
+            if (active) {
+                muse_mode = true;
+            } else {
+                muse_mode = false;
+            }
+    }
+}
+
+
+void matrix_scan_user(void) {
+#ifdef AUDIO_ENABLE
+    if (muse_mode) {
+        if (muse_counter == 0) {
+            uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+            if (muse_note != last_muse_note) {
+                stop_note(compute_freq_for_midi_note(last_muse_note));
+                play_note(compute_freq_for_midi_note(muse_note), 0xF);
+                last_muse_note = muse_note;
+            }
+        }
+        muse_counter = (muse_counter + 1) % muse_tempo;
+    } else {
+        if (muse_counter) {
+            stop_all_notes();
+            muse_counter = 0;
+        }
+    }
+#endif
+}
+
+bool music_mask_user(uint16_t keycode) {
+  switch (keycode) {
+    case RAISE:
+    case LOWER:
+      return false;
+    default:
+      return true;
+  }
+}
