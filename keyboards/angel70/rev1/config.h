541cb9d31a7c89e75941a0b2e0f34aca572af591
ueki5 <ueki005@gmail.com>
2021-10-02 06:51:06 +0900

    元に戻す　8×10

--- keyboards/angel70/rev1/config.h
+++ keyboards/angel70/rev1/config.h
@@ -28,15 +28,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DESCRIPTION     A custom keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 15
-#define MATRIX_COLS 5
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 8
 
 /*
  * Keyboard Matrix Assignments
  *
 */
-#define MATRIX_ROW_PINS { F0, F1, E6, C7, C6, B7, D4, B1, B0, B5, B4, D7, D6, B3, F4 }
-#define MATRIX_COL_PINS { D0, D1, D2, D3, D5 }
+#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
 #define UNUSED_PINS
 
 /*
