72375dd6580531034ae42614f579a97085531bb4
ueki5 <ueki005@gmail.com>
2021-10-02 00:32:36 +0900

    キー数を合わせた

--- keyboards/angel70/rev1/config.h
+++ keyboards/angel70/rev1/config.h
@@ -50,7 +50,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_DI_PIN D3
 #ifdef RGB_DI_PIN
-   #define RGBLED_NUM 64
+   #define RGBLED_NUM 70
    #define RGBLIGHT_HUE_STEP 8
    #define RGBLIGHT_SAT_STEP 8
    #define RGBLIGHT_VAL_STEP 8
