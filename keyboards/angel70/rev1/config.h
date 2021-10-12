8f91ec50b4ff49d534e32def71646cfe85c462fd
ueki5 <ueki005@gmail.com>
2021-10-12 23:35:21 +0900

    TAP:ENT,HOLD:CTRL

--- keyboards/angel70/rev1/config.h
+++ keyboards/angel70/rev1/config.h
@@ -31,6 +31,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 8
 
+// tap and hold
+#define TAPPING_TERM    200
+#define PERMISSIVE_HOLD
+#define RETRO_TAPPING
+
 /*
  * Keyboard Matrix Assignments
  *
